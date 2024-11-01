e29d06ef9eed21675295dddcf8b4de2727d087d3
JKharidinova <haridinova@soft-way.biz>
2024-11-01 20:24:29 +0300

    websocket added

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,11 @@
+import json
 from contextlib import asynccontextmanager
-from typing import Annotated
+from typing import Annotated, List
 
-from fastapi import Query
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 
-from sqlmodel import select
+from sqlmodel import select, SQLModel
 
 from core.db import create_db_and_tables, SessionDep
 from core.models import (
@@ -37,10 +37,40 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+clients: List[WebSocket] = []
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    clients.append(websocket)
+    try:
+        while True:
+            await websocket.receive_text()
+    except WebSocketDisconnect:
+        clients.append(websocket)
+
+
+async def broadcast_event_update(event_data: dict | Event, action: str):
+    if isinstance(event_data, SQLModel):
+        event_data = {
+            "id": event_data.id,
+            "title": event_data.title,
+            "event_dt": event_data.event_dt.isoformat(),
+            "duration": event_data.duration,
+            "location": event_data.location,
+            "organizer_id": event_data.organizer_id,
+            "joiners": [
+                {"id": user.id, "name": user.name} for user in event_data.joiners
+            ] if event_data.joiners else []
+        }
+    message = {"action": action, "data": event_data}
+    for client in clients:
+        await client.send_json(message)
 
 
 @app.post("/users/", response_model=UserPublic)
-def create_user(user: UserCreate, session: SessionDep):
+async def create_user(user: UserCreate, session: SessionDep):
     db_user = User.model_validate(user)
     session.add(db_user)
     session.commit()
@@ -49,7 +79,7 @@ def create_user(user: UserCreate, session: SessionDep):
 
 
 @app.get("/users/", response_model=list[UserPublic])
-def read_users(
+async def read_users(
     session: SessionDep,
     offset: int = 0,
     limit: Annotated[int, Query(le=100)] = 100,
@@ -59,7 +89,7 @@ def read_users(
 
 
 @app.get("/events/", response_model=list[EventPublic])
-def read_events(
+async def read_events(
     session: SessionDep,
     offset: int = 0,
     limit: Annotated[int, Query(le=100)] = 100,
@@ -69,38 +99,41 @@ def read_events(
 
 
 @app.post("/events/", response_model=EventPublic)
-def create_event(event: EventCreate, session: SessionDep):
+async def create_event(event: EventCreate, session: SessionDep):
     db_event = Event.model_validate(event)
     session.add(db_event)
     session.commit()
     session.refresh(db_event)
+    await broadcast_event_update(db_event, "create")
     return db_event
 
 
 @app.post("/event/join/", response_model=EventPublic)
-def join_event(params: M2Mrelation, session: SessionDep):
+async def join_event(params: M2Mrelation, session: SessionDep):
     event = session.get(Event, params.event_id)
     user = session.get(User, params.user_id)
     event.joiners.append(user)
     session.add(event)
     session.commit()
+    await broadcast_event_update(event, "update")
     return event
 
 
 @app.post("/event/quit/", response_model=EventPublic)
-def quit_event(params: M2Mrelation, session: SessionDep):
+async def quit_event(params: M2Mrelation, session: SessionDep):
     event = session.get(Event, params.event_id)
     user = session.get(User, params.user_id)
     user.events.remove(event)
     session.add(event)
     session.commit()
+    await broadcast_event_update(event, "update")
     return event
 
 
 @app.delete("/event/cancel/{event_id}", response_model=dict)
-def cancel_event(event_id: int, session: SessionDep):
+async def cancel_event(event_id: int, session: SessionDep):
     event = session.get(Event, event_id)
     session.delete(event)
     session.commit()
-
+    await broadcast_event_update({"event_id": event_id}, "delete")
     return {"detail": f"Event with ID {event_id} has been deleted"}
